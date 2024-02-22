a558deb4328ed0eed201e5b48ce3167e9e4df347
ilyaTheCreator <respondatanytime@gmail.com>
2024-02-22 16:46:27 +0300

    feat: colorscheme solarized-osaka added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,12 +2,22 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
 -- Keymaps for tmux navigator
-vim.keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", { silent = true })
-vim.keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", { silent = true })
-vim.keymap.set("n", "<C-k>", "<Cmd>TmuxNavigateUp<CR>", { silent = true })
-vim.keymap.set("n", "<C-l>", "<Cmd>TmuxNavigateRight<CR>", { silent = true })
+keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", opts)
+keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", opts)
+keymap.set("n", "<C-k>", "<Cmd>TmuxNavigateUp<CR>", opts)
+keymap.set("n", "<C-l>", "<Cmd>TmuxNavigateRight<CR>", opts)
 
 -- jk kj to exit insert mode
-vim.keymap.set("i", "jk", "<ESC>")
-vim.keymap.set("i", "kj", "<ESC>")
+keymap.set("i", "jk", "<ESC>", opts)
+keymap.set("i", "kj", "<ESC>", opts)
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
