f7fba6b441260fc84f1fac5936c242d05bc7245b
ilyaTheCreator <respondatanytime@gmail.com>
2024-02-22 16:00:00 +0300

    feat: further work with remapping and plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,7 @@ vim.keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", { silent = true })
 vim.keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", { silent = true })
 vim.keymap.set("n", "<C-k>", "<Cmd>TmuxNavigateUp<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", "<Cmd>TmuxNavigateRight<CR>", { silent = true })
+
+-- jk kj to exit insert mode
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "kj", "<ESC>")
